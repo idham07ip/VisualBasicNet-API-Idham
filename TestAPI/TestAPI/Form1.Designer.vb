757d03f54dd370f5671abb1a76cb688ce57501d6
idham07ip <90685644+idham07ip@users.noreply.github.com>
2022-04-02 15:28:11 +0700

    Add files via upload

---
+++ TestAPI/TestAPI/Form1.Designer.vb
@@ -0,0 +1,338 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle7 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle8 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle9 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle10 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle11 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle12 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.nomor = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.npm = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nama = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.jurusan = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.prodi = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.kelas = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btn_tambah = New System.Windows.Forms.Button()
+        Me.btn_cancel = New System.Windows.Forms.Button()
+        Me.btn_delete = New System.Windows.Forms.Button()
+        Me.btn_update = New System.Windows.Forms.Button()
+        Me.btn_add = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txt_kelas = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txt_prodi = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txt_jurusan = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt_nama = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txt_npm = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.nomor, Me.npm, Me.nama, Me.jurusan, Me.prodi, Me.kelas})
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 80)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(897, 264)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'nomor
+        '
+        DataGridViewCellStyle7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nomor.DefaultCellStyle = DataGridViewCellStyle7
+        Me.nomor.HeaderText = "No."
+        Me.nomor.Name = "nomor"
+        Me.nomor.Width = 75
+        '
+        'npm
+        '
+        DataGridViewCellStyle8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.npm.DefaultCellStyle = DataGridViewCellStyle8
+        Me.npm.HeaderText = "Nomor Pokok Mahasiswa"
+        Me.npm.Name = "npm"
+        Me.npm.Width = 175
+        '
+        'nama
+        '
+        DataGridViewCellStyle9.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nama.DefaultCellStyle = DataGridViewCellStyle9
+        Me.nama.HeaderText = "Nama Mahasiswa"
+        Me.nama.Name = "nama"
+        Me.nama.Width = 180
+        '
+        'jurusan
+        '
+        DataGridViewCellStyle10.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.jurusan.DefaultCellStyle = DataGridViewCellStyle10
+        Me.jurusan.HeaderText = "Jurusan"
+        Me.jurusan.Name = "jurusan"
+        Me.jurusan.Width = 155
+        '
+        'prodi
+        '
+        DataGridViewCellStyle11.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.prodi.DefaultCellStyle = DataGridViewCellStyle11
+        Me.prodi.HeaderText = "Prodi"
+        Me.prodi.Name = "prodi"
+        Me.prodi.Width = 200
+        '
+        'kelas
+        '
+        DataGridViewCellStyle12.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.kelas.DefaultCellStyle = DataGridViewCellStyle12
+        Me.kelas.HeaderText = "Kelas"
+        Me.kelas.Name = "kelas"
+        Me.kelas.Width = 75
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.btn_tambah)
+        Me.GroupBox1.Controls.Add(Me.btn_cancel)
+        Me.GroupBox1.Controls.Add(Me.btn_delete)
+        Me.GroupBox1.Controls.Add(Me.btn_update)
+        Me.GroupBox1.Controls.Add(Me.btn_add)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.txt_kelas)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.txt_prodi)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txt_jurusan)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txt_nama)
+        Me.GroupBox1.Controls.Add(Me.DataGridView1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txt_npm)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(2, 6)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1016, 472)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Data Mahasiswa"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(762, 17)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(126, 53)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "Refresh"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btn_tambah
+        '
+        Me.btn_tambah.Location = New System.Drawing.Point(421, 359)
+        Me.btn_tambah.Name = "btn_tambah"
+        Me.btn_tambah.Size = New System.Drawing.Size(133, 32)
+        Me.btn_tambah.TabIndex = 17
+        Me.btn_tambah.Text = "Tambah Data"
+        Me.btn_tambah.UseVisualStyleBackColor = True
+        '
+        'btn_cancel
+        '
+        Me.btn_cancel.Location = New System.Drawing.Point(570, 359)
+        Me.btn_cancel.Name = "btn_cancel"
+        Me.btn_cancel.Size = New System.Drawing.Size(110, 32)
+        Me.btn_cancel.TabIndex = 16
+        Me.btn_cancel.Text = "Cancel"
+        Me.btn_cancel.UseVisualStyleBackColor = True
+        '
+        'btn_delete
+        '
+        Me.btn_delete.Location = New System.Drawing.Point(909, 283)
+        Me.btn_delete.Name = "btn_delete"
+        Me.btn_delete.Size = New System.Drawing.Size(97, 36)
+        Me.btn_delete.TabIndex = 15
+        Me.btn_delete.Text = "Delete"
+        Me.btn_delete.UseVisualStyleBackColor = True
+        '
+        'btn_update
+        '
+        Me.btn_update.Location = New System.Drawing.Point(909, 228)
+        Me.btn_update.Name = "btn_update"
+        Me.btn_update.Size = New System.Drawing.Size(97, 40)
+        Me.btn_update.TabIndex = 14
+        Me.btn_update.Text = "Edit Data"
+        Me.btn_update.UseVisualStyleBackColor = True
+        '
+        'btn_add
+        '
+        Me.btn_add.Location = New System.Drawing.Point(909, 173)
+        Me.btn_add.Name = "btn_add"
+        Me.btn_add.Size = New System.Drawing.Size(97, 36)
+        Me.btn_add.TabIndex = 13
+        Me.btn_add.Text = "Simpan"
+        Me.btn_add.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(872, 416)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(41, 16)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Kelas"
+        '
+        'txt_kelas
+        '
+        Me.txt_kelas.Location = New System.Drawing.Point(867, 435)
+        Me.txt_kelas.Name = "txt_kelas"
+        Me.txt_kelas.Size = New System.Drawing.Size(64, 22)
+        Me.txt_kelas.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(639, 416)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 16)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Prodi"
+        '
+        'txt_prodi
+        '
+        Me.txt_prodi.Location = New System.Drawing.Point(633, 435)
+        Me.txt_prodi.Name = "txt_prodi"
+        Me.txt_prodi.Size = New System.Drawing.Size(218, 22)
+        Me.txt_prodi.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(418, 416)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 16)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Jurusan"
+        '
+        'txt_jurusan
+        '
+        Me.txt_jurusan.Location = New System.Drawing.Point(412, 435)
+        Me.txt_jurusan.Name = "txt_jurusan"
+        Me.txt_jurusan.Size = New System.Drawing.Size(205, 22)
+        Me.txt_jurusan.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(184, 416)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(111, 16)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Nama Mahasiswa"
+        '
+        'txt_nama
+        '
+        Me.txt_nama.Location = New System.Drawing.Point(174, 435)
+        Me.txt_nama.Name = "txt_nama"
+        Me.txt_nama.Size = New System.Drawing.Size(232, 22)
+        Me.txt_nama.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 416)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(156, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Nomor Pokok Mahasiswa"
+        '
+        'txt_npm
+        '
+        Me.txt_npm.Location = New System.Drawing.Point(6, 435)
+        Me.txt_npm.Name = "txt_npm"
+        Me.txt_npm.Size = New System.Drawing.Size(162, 22)
+        Me.txt_npm.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Black", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(269, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 23)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Cari Nama "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(381, 32)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(221, 22)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1030, 484)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txt_jurusan As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txt_nama As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txt_npm As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txt_prodi As System.Windows.Forms.TextBox
+    Friend WithEvents btn_delete As System.Windows.Forms.Button
+    Friend WithEvents btn_update As System.Windows.Forms.Button
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txt_kelas As System.Windows.Forms.TextBox
+    Friend WithEvents btn_cancel As System.Windows.Forms.Button
+    Friend WithEvents btn_add As System.Windows.Forms.Button
+    Friend WithEvents btn_tambah As System.Windows.Forms.Button
+    Friend WithEvents nomor As DataGridViewTextBoxColumn
+    Friend WithEvents npm As DataGridViewTextBoxColumn
+    Friend WithEvents nama As DataGridViewTextBoxColumn
+    Friend WithEvents jurusan As DataGridViewTextBoxColumn
+    Friend WithEvents prodi As DataGridViewTextBoxColumn
+    Friend WithEvents kelas As DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+End Class
---
+++ TestAPI/TestAPI/Form1.vb
@@ -0,0 +1,200 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Web.Script.Serialization
+Imports System.Data.Entity
+
+Public Class Form1
+    Public id As String
+
+    Private Sub UpdateData()
+        Try
+            DataGridView1.Rows.Clear()
+            Dim uriString As String = "http://localhost/ci3_restapi/index.php/Api/PutData/" & txt_npm.Text.Trim & "/" & txt_nama.Text.Trim & "/" & txt_jurusan.Text.Trim & "/" & txt_prodi.Text.Trim & "/" & txt_kelas.Text.Trim
+            Dim uri As New Uri(uriString)
+            Dim request As HttpWebRequest = HttpWebRequest.Create(uri)
+            request.Method = "PUT"
+            Dim response As HttpWebResponse = request.GetResponse()
+            Dim read = New StreamReader(response.GetResponseStream())
+            Dim raw As String = read.ReadToEnd()
+            Dim dict As Object = New JavaScriptSerializer().Deserialize(Of List(Of Object))(raw)
+            For Each item As Object In dict
+                DataGridView1.Rows.Add(item("npm").ToString, item("nama").ToString, item("jurusan").ToString,
+                                       item("prodi").ToString, item("kelas").ToString)
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Notifikasi")
+        End Try
+
+    End Sub
+
+
+    Private Sub HapusData()
+        Try
+            DataGridView1.Rows.Clear()
+            Dim uriString As String = "http://localhost/ci3_restapi/index.php/Api/DeleteData/" & txt_npm.Text.Trim & "/" & txt_nama.Text.Trim & "/" & txt_jurusan.Text.Trim & "/" & txt_prodi.Text.Trim & "/" & txt_kelas.Text.Trim
+            Dim uri As New Uri(uriString)
+            Dim request As HttpWebRequest = HttpWebRequest.Create(uri)
+            request.Method = "DELETE"
+            Dim response As HttpWebResponse = request.GetResponse()
+            Dim read = New StreamReader(response.GetResponseStream())
+            Dim raw As String = read.ReadToEnd()
+            Dim dict As Object = New JavaScriptSerializer().Deserialize(Of List(Of Object))(raw)
+            For Each item As Object In dict
+                DataGridView1.Rows.Add(item("npm").ToString, item("nama").ToString, item("jurusan").ToString,
+                                       item("prodi").ToString, item("kelas").ToString)
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Notifikasi")
+        End Try
+
+    End Sub
+    Private Sub tambahdata()
+        Try
+            DataGridView1.Rows.Clear()
+            Dim uriString As String = "http://localhost/ci3_restapi/index.php/Api/PostData/" & txt_npm.Text.Trim & "/" & txt_nama.Text.Trim & "/" & txt_jurusan.Text.Trim & "/" & txt_prodi.Text.Trim & "/" & txt_kelas.Text.Trim
+            Dim uri As New Uri(uriString)
+            Dim request As HttpWebRequest = HttpWebRequest.Create(uri)
+            request.Method = "POST"
+            Dim response As HttpWebResponse = request.GetResponse()
+            Dim read = New StreamReader(response.GetResponseStream())
+            Dim raw As String = read.ReadToEnd()
+            Dim dict As Object = New JavaScriptSerializer().Deserialize(Of List(Of Object))(raw)
+            For Each item As Object In dict
+                DataGridView1.Rows.Add(item("npm").ToString, item("nama").ToString, item("jurusan").ToString,
+                                       item("prodi").ToString, item("kelas").ToString)
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Notifikasi")
+        End Try
+
+    End Sub
+
+    Private Sub carinama()
+        Try
+            DataGridView1.Rows.Clear()
+            Dim uriString As String = "http://localhost/ci3_restapi/index.php/Api/SearchData/" & TextBox1.Text.Trim
+            Dim uri As New Uri(uriString)
+            Dim request As HttpWebRequest = HttpWebRequest.Create(uri)
+            request.Method = "GET"
+            Dim response As HttpWebResponse = request.GetResponse()
+            Dim read = New StreamReader(response.GetResponseStream())
+            Dim raw As String = read.ReadToEnd()
+            Dim dict As Object = New JavaScriptSerializer().Deserialize(Of List(Of Object))(raw)
+            For Each item As Object In dict
+                DataGridView1.Rows.Add(item("id").ToString, item("npm").ToString, item("nama").ToString, item("jurusan").ToString,
+                                       item("prodi").ToString, item("kelas").ToString)
+            Next
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+    Private Sub getdata()
+        Try
+
+            Dim uriString As String = "http://localhost/ci3_restapi/index.php/Api/GetData"
+            Dim uri As New Uri(uriString)
+            Dim request As HttpWebRequest = HttpWebRequest.Create(uri)
+            request.Method = "GET"
+            Dim response As HttpWebResponse = request.GetResponse()
+            Dim read = New StreamReader(response.GetResponseStream())
+            Dim raw As String = read.ReadToEnd()
+            Dim dict As Object = New JavaScriptSerializer().Deserialize(Of List(Of Object))(raw)
+            For Each item As Object In dict
+                DataGridView1.Rows.Add(item("id").ToString, item("npm").ToString, item("nama").ToString, item("jurusan").ToString,
+                                       item("prodi").ToString, item("kelas").ToString)
+
+            Next
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Notifikasi")
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        getdata()
+    End Sub
+
+
+    Private Sub btn_add_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_add.Click
+        tambahdata()
+        getdata()
+
+        txt_npm.Text = ""
+        txt_nama.Text = ""
+        txt_jurusan.Text = ""
+        txt_prodi.Text = ""
+        txt_kelas.Text = ""
+    End Sub
+
+    Private Sub btn_delete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_delete.Click
+        HapusData()
+        getdata()
+
+        txt_npm.Text = ""
+        txt_nama.Text = ""
+        txt_jurusan.Text = ""
+        txt_prodi.Text = ""
+        txt_kelas.Text = ""
+    End Sub
+
+    Private Sub btn_update_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_update.Click
+        UpdateData()
+        getdata()
+
+        txt_npm.Text = ""
+        txt_nama.Text = ""
+        txt_jurusan.Text = ""
+        txt_prodi.Text = ""
+        txt_kelas.Text = ""
+    End Sub
+
+    Private Sub DataGridView1_CellDoubleClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellDoubleClick
+        id = Nothing
+        id = DataGridView1.Rows(e.RowIndex).Cells(0).Value
+        txt_npm.Text = DataGridView1.Rows(e.RowIndex).Cells(1).Value
+        txt_nama.Text = DataGridView1.Rows(e.RowIndex).Cells(2).Value
+        txt_jurusan.Text = DataGridView1.Rows(e.RowIndex).Cells(3).Value
+        txt_prodi.Text = DataGridView1.Rows(e.RowIndex).Cells(4).Value
+        txt_kelas.Text = DataGridView1.Rows(e.RowIndex).Cells(5).Value
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        id = Nothing
+        id = DataGridView1.Rows(e.RowIndex).Cells(0).Value
+        txt_npm.Text = DataGridView1.Rows(e.RowIndex).Cells(1).Value
+        txt_nama.Text = DataGridView1.Rows(e.RowIndex).Cells(2).Value
+        txt_jurusan.Text = DataGridView1.Rows(e.RowIndex).Cells(3).Value
+        txt_prodi.Text = DataGridView1.Rows(e.RowIndex).Cells(4).Value
+        txt_kelas.Text = DataGridView1.Rows(e.RowIndex).Cells(5).Value
+    End Sub
+
+    Private Sub btn_tambah_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_tambah.Click
+        txt_npm.Text = ""
+        txt_nama.Text = ""
+        txt_jurusan.Text = ""
+        txt_prodi.Text = ""
+        txt_kelas.Text = ""
+        txt_npm.Focus()
+    End Sub
+
+    Private Sub btn_cancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_cancel.Click
+        txt_npm.Text = ""
+        txt_nama.Text = ""
+        txt_jurusan.Text = ""
+        txt_prodi.Text = ""
+        txt_kelas.Text = ""
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        carinama()
+        If TextBox1.Text Is "" Then
+            getdata()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.DataGridView1.Rows.Clear()
+        getdata()
+    End Sub
+End Class
